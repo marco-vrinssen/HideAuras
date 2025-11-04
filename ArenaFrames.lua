5d7b4f508db332ad166923f0edc33fb39a820597
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-11-04 19:38:16 +0100

    Improve naming convention for better readability
    - Rename hide to hideElement for clarity
    - Rename frame to arenaFrame for context
    - Rename index to arenaIndex for context
    - Add local variables: debuffs, ccRemover, castBar
    - Remove plural from function name

--- ArenaFrames.lua
+++ ArenaFrames.lua
@@ -1,25 +1,56 @@
--- Hides auras on arena frames
+-- Customizes arena frame elements for better visibility
 
-local function hideFrameElementPermanently(frameElement)
-	if frameElement then
-		frameElement:Hide()
-		frameElement:SetScript("OnShow", frameElement.Hide)
+local function hideElement(element)
+	if element then
+		element:Hide()
+		element:SetScript("OnShow", element.Hide)
 	end
 end
 
-local function hideArenaFrameElements()
-	for arenaFrameIndex = 1, 5 do
-		local compactArenaFrame = _G["CompactArenaFrameMember" .. arenaFrameIndex]
-		if compactArenaFrame then
-			hideFrameElementPermanently(compactArenaFrame.name)
-			hideFrameElementPermanently(compactArenaFrame.DebuffFrame)
-			hideFrameElementPermanently(compactArenaFrame.castBar)
+local function customizeArenaFrame()
+	for arenaIndex = 1, 5 do
+		local arenaFrame = _G["CompactArenaFrameMember" .. arenaIndex]
+		if arenaFrame then
+
+			-- Hide name
+			hideElement(arenaFrame.name)
+
+			-- Position and resize debuff frame
+			local debuffs = arenaFrame.DebuffFrame
+			if debuffs then
+				debuffs:ClearAllPoints()
+				debuffs:SetSize(28, 28)
+				debuffs:SetPoint("BOTTOMLEFT", arenaFrame, "BOTTOMLEFT", -2, 2)
+			end
+
+			-- Position and resize CC remover frame
+			local ccRemover = arenaFrame.CcRemoverFrame
+			if ccRemover then
+				ccRemover:ClearAllPoints()
+				ccRemover:SetSize(28, 28)
+				ccRemover:SetPoint("TOPLEFT", arenaFrame, "TOPRIGHT", -2, 0)
+
+				-- Zoom CC remover icon
+				if ccRemover.Icon then
+					ccRemover.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+				end
+			end
+
+			-- Customize casting bar
+			local castBar = arenaFrame.CastingBarFrame
+			if castBar then
+				hideElement(castBar.Text)
+				hideElement(castBar.TextBorder)
+				hideElement(castBar.Icon)
+
+				castBar:ClearAllPoints()
+				castBar:SetPoint("BOTTOMRIGHT", arenaFrame, "BOTTOMLEFT")
+			end
+
 		end
 	end
 end
 
 local arenaEventFrame = CreateFrame("Frame")
 arenaEventFrame:RegisterEvent("ARENA_OPPONENT_UPDATE")
-arenaEventFrame:SetScript("OnEvent", function()
-	hideArenaFrameElements()
-end)
+arenaEventFrame:SetScript("OnEvent", customizeArenaFrame)
